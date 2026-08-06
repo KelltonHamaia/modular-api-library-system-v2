bd7564b13acfa7b2723149640ccfcf5c502f1f37
Kellton M.H.A <kellhamaiaa@gmail.com>
2026-08-05 23:09:12 -0300

    feat: add .env validation

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { app } from '@/app.js'
+import { env } from '@/config/env.config.js'
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-  console.log(`API running at port ${PORT}`)
+app.listen(env.PORT, () => {
+  console.log(`API running at port ${env.PORT}`)
 })
