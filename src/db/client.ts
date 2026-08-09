20cbbe46e14404f0f97b4781d2f82f28ff9cb6f6
Kellton M.H.A <kellhamaiaa@gmail.com>
2026-08-09 19:46:31 -0300

    fix: insert the correct type to DBExecutor

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,5 +1,4 @@
 import { env } from '@/config/env.config.js'
-import { drizzle } from 'drizzle-orm/node-postgres'
-
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres'
 export const db = drizzle(env.DATABASE_URL)
-export type DBExecutor = typeof db
+export type DBExecutor = NodePgDatabase
